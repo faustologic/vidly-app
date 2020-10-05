25047102141177173c6ccf3f14b3e362cf3fae0d
faustologic <69010388+faustologic@users.noreply.github.com>
2020-10-04 23:43:36 -0500

    Like icon button
    We create a new like button using font awesome icons, and making a common component using onClick events and a stateless function component.

---
+++ src/common/like.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+// input: liked: Boolean
+// Output: onClick
+// Stateless Function component
+
+const Like = (props) => {
+  let classes = "fa fa-heart";
+  if (!props.liked) classes += "-o";
+  return (
+    <i
+      onClick={props.onClick}
+      style={{ cursor: "pointer" }}
+      className={classes}
+      aria-hidden="true"
+    />
+  );
+};
+
+export default Like;
