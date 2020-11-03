e99918997169cba3020fa9034fbe1a6062298308
faustologic <69010388+faustologic@users.noreply.github.com>
2020-11-02 19:39:42 -0500

    Schema Joi
    Joi and schema updated, also we create a new for of loop to iterate on the forms errors

--- src/components/loginForm.jsx
+++ src/components/loginForm.jsx
@@ -8,24 +8,21 @@ class LoginForm extends Component {
     errors: {},
   };
 
+  // the schema it's not goint to change, so that`s why we don't put it on the state objects.
   schema = {
-    username: Joi.string().required(),
-    password: Joi.string().required(),
+    username: Joi.string().required().label("Username"),
+    password: Joi.string().required().label("Password"),
   };
 
   validate = () => {
-    const result = Joi.validate(this.state.account, this.schema, {
-      abortEarly: false,
-    });
-    console.log(result);
+    const options = { abortEarly: false };
+    const { error } = Joi.validate(this.state.account, this.schema, options);
+
+    if (!error) return null;
 
     const errors = {};
-    if (this.state.account.username.trim() === "")
-      errors.username = "Username is required!";
-    if (this.state.account.password.trim() === "")
-      errors.password = "Password is required!";
-
-    return Object.keys(errors).length === 0 ? null : errors;
+    for (let item of error.details) errors[item.path[0]] = item.message;
+    return errors;
   };
 
   handleSubmit = (e) => {
