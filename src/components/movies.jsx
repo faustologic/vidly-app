ff6f603f261284d47016e59616c25eee4b3563ab
faustologic <69010388+faustologic@users.noreply.github.com>
2021-01-09 18:46:44 -0500

    Initial commit

--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -106,7 +106,7 @@ class Movies extends Component {
   };
 
   render() {
-    const { length: count } = this.state.movies;
+    // const { length: count } = this.state.movies;
     const { pageSize, currentPage, sortColumn, searchQuery } = this.state;
     const { user } = this.props;
     const { totalCount, data: movies } = this.getPagedRender();
